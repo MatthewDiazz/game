1556cc3e15ac971ad087960452ac60fb51c8653e
MatthewDiazz <113604049+MatthewDiazz@users.noreply.github.com>
2023-02-15 08:55:54 -0500

    Add files via upload

--- pygame/coin_game.py
+++ pygame/coin_game.py
@@ -1,3 +1,5 @@
+from random import randint
+
 WIDTH = 400
 HEIGHT = 400
 score = 0
@@ -12,33 +14,38 @@ def draw():
     screen.fill("green")
     character.draw()
     coin.draw()
-    screen.draw.text(("Score: " + str(score), color="black", topleft=(10, 10))
+    screen.draw.text("Score: " + str(score), color="black", topleft=(10, 10))
 
     if game_over:
         screen.fill("blue")
-        screen.draw.text("Final score: " + str (score), topleft=(10, 10), fontsize=60
+        screen.draw.text("Final score: " + str (score), topleft=(10, 10), fontsize=60)
 
 def place_coin():
     coin.x = randint(20, (WIDTH - 20))
     coin.y = randint(20, (HEIGHT - 20))
+
 def time_up():
     global game_over
     game_over = True
-clock.schedule(time_up, 7.0)
+
 
 def update():
     global score
 
     if keyboard.left:
-        character.x = character.x - 2
+        character.x = character.x - 10
     elif keyboard.right:
-        character.x = character.x + 2
+        character.x = character.x + 10
     elif keyboard.up:
-        character.y = character.y - 2
+        character.y = character.y - 10
     elif keyboard.down:
-        charcter.y = character.y + 2
+        character.y = character.y + 10
 
     coin_collected = character.colliderect(coin)
 
     if coin_collected:
-        score =
+        score = score + 67868767672346776976953476457691459645136794567945136793415679459674569514
+        place_coin()
+
+clock.schedule(time_up, 100.0)
+place_coin()
