8398a2feef1f7251d145f4f48ba39e434347363c
MatthewDiazz <113604049+MatthewDiazz@users.noreply.github.com>
2023-02-15 08:55:09 -0500

    Add files via upload

---
+++ game-main/pygame/click_game.py
@@ -0,0 +1,36 @@
+character = Actor('character')
+character.pos = 10, 50
+
+WIDTH = 500
+HEIGHT = character.height + 20
+
+def draw():
+    screen.clear ()
+    character.draw ()
+
+def update():
+    character.left = character.left + 2
+    if character.right > WIDTH:
+        character.right = 0
+
+def on_mouse_down(pos):
+    if character.collidepoint(pos):
+        print("Eek!")
+
+def on_mouse_click(pos):
+    if character.collidepoint(pos):
+        print("Eek!")
+    else:
+        print("you missed me!")
+
+def on_mouse_down():
+    print("You clicked!")
+
+def on_mouse_down(pos, button):
+    if button == mouse.LEFT and character.collidepoint(pos):
+        print("Eek!")
+
+def on_mouse_down(pos):
+    if character.collidepoint(pos):
+        sounds.eep.play()
+        character.image = 'clickgamecharacter'
